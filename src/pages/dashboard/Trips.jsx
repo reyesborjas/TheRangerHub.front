86c4b39e5160e0bba9a51a3d5620d513d489c653
dcortesnet <dcortes.net@gmail.com>
2025-02-21 21:26:45 -0300

    Se agrega menu para buscar un viaje

--- src/pages/dashboard/Trips.jsx
+++ src/pages/dashboard/Trips.jsx
@@ -4,12 +4,12 @@ export const Trips = () => {
   let [trips, setTrips] = useState([]);
 
   useEffect(() => {
-    fetch("")
+    fetch("https://rangerhub-back.vercel.app/trips")
       .then((response) => {
         return response.json();
       })
       .then((responseConverted) => {
-        console.log(responseConverted);
+        setTrips(responseConverted.trips);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -17,6 +17,48 @@ export const Trips = () => {
   return (
     <div>
       <h1>Página de viajes</h1>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          {trips.length > 0 &&
+            trips.map((trip) => {
+              return (
+                <div className="col-4">
+                  <div className="card">
+                    <img
+                      src={trip.trip_image_url}
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h3 className="card-title">{trip.trip_name}</h3>
+                      <h5>Precio ${trip.total_cost}</h5>
+                      <p className="card-text">{trip.description}</p>
+                      <a href="#" className="btn btn-primary">
+                        Lo quiero!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
