647bb17dc7c6243083cbbea9cafbd32c53526935
Clara Espinosa Pantoja <claritapantoja36@gmail.com>
2025-02-28 21:24:48 +0000

    dashboard

--- src/components/SideBarDashboard.jsx
+++ src/components/SideBarDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.svg";
 import "../styles/SideBarDashboard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
