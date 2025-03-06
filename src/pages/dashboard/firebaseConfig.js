c7e767759d84f7b1c4fa3dfc15a134530705f23f
reyesborjas <reyesborjas@gmail.com>
2025-03-05 22:59:04 -0300

    payment implementation incomplete

---
+++ src/pages/dashboard/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+// Obtener referencia al servicio de almacenamiento
+const storage = getStorage(app);
+
+export { storage, ref, uploadBytes, getDownloadURL };
